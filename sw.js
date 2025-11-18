83b2dfe140a8fa052ff008a3b77e19d2cac4a0cd
dgmacctg1999-cell <dgmacctg1999@gmail.com>
2025-11-18 16:27:35 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,19 @@
-self.addEventListener("install", (e) => {
-e.waitUntil(
-caches.open("pwa-pos-cache-v1").then((cache) => {
-return cache.addAll([
-"/",
-"/index.html",
-"/manifest.json",
-"/icon-192.png",
-"/icon-512.png"
-]);
-})
-);
-self.skipWaiting();
-});
-
-
-self.addEventListener("activate", (e) => {
-e.waitUntil(caches.keys().then((keys) => {
-return Promise.all(
-keys.filter((k) => k !== "pwa-pos-cache-v1").map((k) => caches.delete(k))
-);
-}));
-});
-
-
-self.addEventListener("fetch", (e) => {
-e.respondWith(
-caches.match(e.request).then((cacheRes) => {
-return cacheRes || fetch(e.request);
-})
-);
-});const CACHE_NAME = "hardware-pos-v1";
+const CACHE_NAME = 'hardware-pos-v1';
 const FILES = [
-"./",
-"index.html",
-"app.js",
-"manifest.json"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
-
-
-self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-);
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
 });
-
-
-self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(resp => resp || fetch(event.request))
-);
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(resp=>resp || fetch(e.request).catch(()=>caches.match('index.html'))));
 });
